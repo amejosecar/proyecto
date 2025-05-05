054407fcf468e729e572c9ff559a943365ebde7d
amejosecar <amejosecar@hotmail.com>
2025-05-05 09:43:57 +0200

    Lunes 05mayo2025 actualizar documentos

--- proy_app/new_pro_app/src/App.jsx
+++ proy_app/new_pro_app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-//import "./App.css";
+import "./App.css";
 import BasicAccordion from "./Accordion"; // Importamos el componente desde Accordion.jsx
 
 function App() {
